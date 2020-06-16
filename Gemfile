4bd9c372c732dbd9bf5d168f75ed6a6966f2da7c
Tiffany Charles <charles.tiffany.r@gmail.com>
2020-06-16 01:56:10 +0100

    deploying

--- Gemfile
+++ Gemfile
@@ -4,7 +4,10 @@ gem 'middleman', '~> 4.3.6'
 gem 'middleman-autoprefixer', '~> 2.7'
 # gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby, :x64_mingw]
 # gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw, :x64_mingw]
-gem "activesupport", ">= 5.2.4.3"
+gem "activesupport", ">= 5.2.4"
 
 
 gem 'cloudinary', '~> 1.12.0'
+
+
+gem "middleman-deploy", "~> 2.0.0.pre.alpha"
--- config.rb
+++ config.rb
@@ -16,6 +16,15 @@ page '/*.txt', layout: false
 activate :directory_indexes
 
 
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  # Optional Settings
+  # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
+  # deploy.branch   = 'custom-branch' # default: gh-pages
+  # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
+  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+end
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
